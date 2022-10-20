429d97382074b8876285439e3f84f00bc976a947
phamvanda123 <114846809+phamvanda123@users.noreply.github.com>
2022-10-20 12:46:24 +0700

    upload

---
+++ main/js/script.js
@@ -0,0 +1,6 @@
+var a = document.getElementById("label")
+a.addEventListener("click", function() {
+    document.getElementById("myid").style.display = 'block';
+    document.getElementById("myid1").style.display = 'block';
+    document.getElementById("myid2").style.display = 'block';
+})
